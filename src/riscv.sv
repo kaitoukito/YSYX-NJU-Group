a8b86a9d8b24bfb292f512b2ac11dcf30d4c17d9
kaitoukito <675202332@qq.com>
2021-07-09 19:19:31 -0700

    init

--- src/riscv.sv
+++ src/riscv.sv
@@ -1,15 +1,19 @@
 /// RV 64I Base Integer Instruction Set
 module riscv #(
-    parameter   IBUS_DATA_WIDTH = 32,           // instruction bus data width
-    parameter   DBUS_DATA_WIDTH = 64,           // data bus data width
-    parameter   RF_ADDR_WIDTH   = $clog(32),    // register file address width
-    parameter   IMEM_ADDR_WIDTH = $clog(4096),  // instruction memory address width
-    parameter   DMEM_ADDR_WIDTH = $clog(16384)  // data memory address width
+    parameter   IBUS_DATA_WIDTH = 32,               // instruction bus data width
+    parameter   DBUS_DATA_WIDTH = 64,               // data bus data width
+    parameter   RF_ADDR_WIDTH   = $clog2(32),       // register file address width
+    parameter   IMEM_ADDR_WIDTH = $clog2(4096),     // instruction memory address width
+    parameter   DMEM_ADDR_WIDTH = $clog2(16384)     // data memory address width
 ) (
     input   logic   clk,
     input   logic   rst_n,      // asynchronous reset
     input   logic   sft_rst     // synchronous reset
 );
+
+    //----------------------------------------
+    // Declarations
+    //----------------------------------------
 
     logic   [DBUS_DATA_WIDTH-1:0]   pc;
 
@@ -27,14 +31,13 @@ module riscv #(
     logic                           mem_write_id_stage;
     logic                           branch_id_stage;
     logic   [1:0]                   alu_op_id_stage;
-    logic   [RF_ADDR_WIDTH-1:0]     rd1_addr_id_stage;
-    logic   [RF_ADDR_WIDTH-1:0]     rd2_addr_id_stage;
-    logic   [RF_ADDR_WIDTH-1:0]     wr_addr_id_stage;
-    logic   [DBUS_DATA_WIDTH-1:0]   wr_data_id_stage;
+    logic   [RF_ADDR_WIDTH-1:0]     rf_rd1_addr_id_stage;
+    logic   [RF_ADDR_WIDTH-1:0]     rf_rd2_addr_id_stage;
 
     // ID 2 EX FFs
-    logic   [DBUS_DATA_WIDTH-1:0]   rd1_data_id2ex_ff;
-    logic   [DBUS_DATA_WIDTH-1:0]   rd2_data_id2ex_ff;
+    logic   [DBUS_DATA_WIDTH-1:0]   rf_rd1_data_id2ex_ff;
+    logic   [DBUS_DATA_WIDTH-1:0]   rf_rd2_data_id2ex_ff;
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_id2ex_ff;
 
     // EX nets
     logic   [DBUS_DATA_WIDTH-1:0]   a_ex_stage;
@@ -55,22 +58,49 @@ module riscv #(
     logic   [DBUS_DATA_WIDTH-1:0]   alu_out_mem2wb_ff;
 
     // WB nets
+    logic   [RF_ADDR_WIDTH-1:0]     rf_wr_addr_wb_stage;
+    logic                           rf_wr_en_wb_stage;
+    logic   [DBUS_DATA_WIDTH-1:0]   rf_wr_data_wb_stage;
+
+    //----------------------------------------
+    // Implementations
+    //----------------------------------------
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pc <= 'd0;
+        end
+        else if (sft_rst) begin
+            pc <= 'd0;
+        end
+        else begin
+            pc <= pc + 4;
+        end
+    end
 
     //----------------------------------------
     // IF stage
     //----------------------------------------
 
-    // instruction memory, totally 32KiB
+    // instruction memory
     riscv_ram #(
         .DATA_WIDTH (DBUS_DATA_WIDTH    ),
         .ADDR_WIDTH (IMEM_ADDR_WIDTH    )
     ) U_RISCV_IMEM (
         .clk        (clk                ),
-        .cs         (),
-        .we         (),
+        .cs         (1'b1               ),
+        .we         (1'b0               ),
         .addr       (pc[IMEM_ADDR_WIDTH-1:0]),
-        .wr_data    (),
+        .wr_data    ('0                 ),
         .rd_data    (instr_if2id_ff     )
+    );
+
+    riscv_adder #(
+        .DATA_WIDTH (DBUS_DATA_WIDTH    )
+    ) U_RISCV_ADDER_IF (
+        .a          (),
+        .b          (),
+        .s          ()
     );
 
     //----------------------------------------
@@ -90,20 +120,31 @@ module riscv #(
         .alu_op     (alu_op_id_stage    )  // O
     )
 
+    assign rf_rd1_addr_id_stage = instr_if2id_ff[19:15];
+    assign rf_rd2_addr_id_stage = instr_if2id_ff[24:20];
+
     riscv_rf #(
         .DATA_WIDTH (DBUS_DATA_WIDTH    ),
         .ADDR_WIDTH (RF_ADDR_WIDTH      )
     ) U_RISCV_RF (
         .clk        (clk                ),
-        .rd1_addr   (rd1_addr_id_stage  ),
-        .rd1_en     (),
-        .rd1_data   (rd1_data_id2ex_ff  ),
-        .rd2_addr   (rd2_addr_id_stage  ),
-        .rd2_en     (),
-        .rd2_data   (rd2_data_id2ex_ff  ),
-        .wr_addr    (wr_addr_id_stage   ),
-        .wr_en      (),
-        .wr_data    (wr_data_id_stage   )
+        .rd1_addr   (rf_rd1_addr_id_stage),
+        .rd1_en     (1'b1               ),
+        .rd1_data   (rf_rd1_data_id2ex_ff),
+        .rd2_addr   (rf_rd2_addr_id_stage),
+        .rd2_en     (1'b1               ),
+        .rd2_data   (rf_rd2_data_id2ex_ff),
+        .wr_addr    (rf_wr_addr_wb_stage),
+        .wr_en      (rf_wr_en_wb_stage  ),
+        .wr_data    (rf_wr_data_wb_stage)
+    );
+
+    riscv_imm_gen #(
+        .IBUS_DATA_WIDTH(IBUS_DATA_WIDTH),
+        .DBUS_DATA_WIDTH(DBUS_DATA_WIDTH)
+) (
+        .instr      (instr_if2id_ff     ), // I
+        .imm        (imm_id2ex_ff       )  // O
 );
 
     //----------------------------------------
@@ -120,11 +161,19 @@ module riscv #(
         .zero       (zero_ex_stage      )
     )
 
+    riscv_adder #(
+        .DATA_WIDTH (DBUS_DATA_WIDTH    )
+    ) U_RISCV_ADDER_EX (
+        .a          (),
+        .b          (),
+        .s          ()
+    );
+
     //----------------------------------------
     // MEM stage
     //----------------------------------------
 
-    // data memory, totally 128KiB
+    // data memory
     riscv_ram #(
         .DATA_WIDTH (DBUS_DATA_WIDTH    ),
         .ADDR_WIDTH (DMEM_ADDR_WIDTH    )
--- src/riscv_imm_gen.sv
+++ src/riscv_imm_gen.sv
@@ -1,7 +1,104 @@
-module riscv_imm_gen(
-
+module riscv_imm_gen #(
+    parameter   IBUS_DATA_WIDTH = -1,
+    parameter   DBUS_DATA_WIDTH = -1
 ) (
+    input   logic   [IBUS_DATA_WIDTH-1:0]   instr,
+    output  logic   [DBUS_DATA_WIDTH-1:0]   imm
+);
 
-)
+    logic   opcode;
+
+    logic   is_rtype;
+    logic   is_itype;
+    logic   is_stype;
+    logic   is_btype;
+    logic   is_utype;
+    logic   is_jtype;
+
+    logic   [11:0]  imm_itype;
+    logic   [11:0]  imm_stype;
+    logic   [12:1]  imm_btype;
+    logic   [31:12] imm_utype;
+    logic   [20:1]  imm_jtype;
+
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_sext_itype;
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_sext_stype;
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_sext_btype;
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_sext_utype;
+    logic   [DBUS_DATA_WIDTH-1:0]   imm_sext_jtype;
+
+    // TODO
+    assign opcode = instr[6:0];
+
+    assign is_rtype;
+    assign is_itype;
+    assign is_stype;
+    assign is_btyep;
+    assign is_utype;
+    assign is_jtype;
+
+    assign imm_itype = instr[31:20];
+    assign imm_stype = {instr[31:25], instr[11:7]};
+    assign imm_btype = {instr[31], instr[7], instr[30:25], instr[11:8]};
+    assign imm_utype = instr[31:12];
+    assign imm_jtype = {instr[31], instr[19:12], instr[20], instr[30:21]};
+
+    riscv_ext #(
+        .IS_SIGNED      (1              ),
+        .DATA_WIDTH_I   (12             ),
+        .DATA_WIDTH_O   (DBUS_DATA_WIDTH)
+    ) U_RISCV_SEXT_ITYPE (
+        .data_i         (imm_itype      ),
+        .data_o         (imm_sext_itype )
+    );
+
+    riscv_ext #(
+        .IS_SIGNED      (1              ),
+        .DATA_WIDTH_I   (12             ),
+        .DATA_WIDTH_O   (DBUS_DATA_WIDTH)
+    ) U_RISCV_SEXT_STYPE (
+        .data_i         (imm_stype      ),
+        .data_o         (imm_sext_stype )
+    );
+
+    riscv_ext #(
+        .IS_SIGNED      (1              ),
+        .DATA_WIDTH_I   (12             ),
+        .DATA_WIDTH_O   (DBUS_DATA_WIDTH)
+    ) U_RISCV_SEXT_BTYPE (
+        .data_i         (imm_btype      ),
+        .data_o         (imm_sext_btype )
+    );
+
+    riscv_ext #(
+        .IS_SIGNED      (1              ),
+        .DATA_WIDTH_I   (20             ),
+        .DATA_WIDTH_O   (DBUS_DATA_WIDTH)
+    ) U_RISCV_SEXT_UTYPE (
+        .data_i         (imm_utype      ),
+        .data_o         (imm_sext_utype )
+    );
+
+    riscv_ext #(
+        .IS_SIGNED      (1              ),
+        .DATA_WIDTH_I   (20             ),
+        .DATA_WIDTH_O   (DBUS_DATA_WIDTH)
+    ) U_RISCV_SEXT_JTYPE (
+        .data_i         (imm_jtype      ),
+        .data_o         (imm_sext_jtype )
+    );
+
+    // one-hot code
+    always_comb begin
+        case ({is_jtype, is_utype, is_btyep, is_stype, is_itype, is_rtype})
+            6'b00_0001  :   imm = 'd0;
+            6'b00_0010  :   imm = imm_sext_itype;
+            6'b00_0100  :   imm = imm_sext_stype;
+            6'b00_1000  :   imm = imm_sext_btype;
+            6'b01_0000  :   imm = imm_sext_utype;
+            6'b10_0000  :   imm = imm_sext_jtype;
+            default     :   imm = 'd0;
+        endcase
+    end
 
 endmodule
