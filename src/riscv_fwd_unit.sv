b2b7d7c18ac7b2ffe6aa0f27681dd6443ebeaf53
kaitoukito <675202332@qq.com>
2021-07-10 09:35:14 -0700

    init

--- src/riscv_fwd_unit.sv
+++ src/riscv_fwd_unit.sv
@@ -16,33 +16,25 @@ module riscv_fwd_unit #(
 );
 
     // blocked assignment
+    // the second if-statement will cover the first one, if condition is true
     always_comb begin
         fwd_a = 2'b00;
-        if (reg_write_ex2mem_ff
-        && (rd_ex2mem_ff != 'd0)
-        && (rd_ex2mem_ff == rs1_id2ex_ff)) begin
-            fwd_a = 2'b10;
-        end
-        if (reg_write_mem2wb_ff
-        && (rd_mem2wb_ff != 'd0)
-        && ~(reg_write_ex2mem_ff && (rd_ex2mem_ff != 0) && (rd_ex2mem_ff == rs1_id2ex_ff))
-        && (rd_mem2wb_ff == rs1_id2ex_ff)) begin
+        if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs1_id2ex_ff)) begin
             fwd_a = 2'b01;
+        end
+        if (reg_write_ex2mem_ff && (rd_ex2mem_ff != 'd0) && (rd_ex2mem_ff == rs1_id2ex_ff)) begin
+            fwd_a = 2'b10;
         end
     end
 
     // blocked assignment
+    // the second if-statement will cover the first one, if condition is true
     always_comb begin
         fwd_b = 2'b00;
-        if (reg_write_ex2mem_ff
-        && (rd_ex2mem_ff != 'd0)
-        && (rd_ex2mem_ff == rs2_id2ex_ff)) begin
-            fwd_b = 2'b10;
+        if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs2_id2ex_ff)) begin
+            fwd_b = 2'b01;
         end
-        if (reg_write_mem2wb_ff
-        && (rd_mem2wb_ff != 'd0)
-        && ~(reg_write_ex2mem_ff && (rd_ex2mem_ff != 0) && (rd_ex2mem_ff == rs2_id2ex_ff))
-        && (rd_mem2wb_ff == rs2_id2ex_ff)) begin
+        if (reg_write_ex2mem_ff && (rd_ex2mem_ff != 'd0) && (rd_ex2mem_ff == rs2_id2ex_ff)) begin
             fwd_b = 2'b10;
         end
     end
