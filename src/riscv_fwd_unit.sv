59b981b6e7898df52e403842b7072801959da06d
kaitoukito <675202332@qq.com>
2021-07-14 05:15:01 -0700

    init

--- src/riscv_fwd_unit.sv
+++ src/riscv_fwd_unit.sv
@@ -15,27 +15,27 @@ module riscv_fwd_unit #(
     output  logic   [1:0]               fwd_b
 );
 
-    // blocked assignment
-    // the second if-statement will cover the first one, if condition is true
     always_comb begin
-        fwd_a = 2'b00;
-        if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs1_id2ex_ff)) begin
-            fwd_a = 2'b01;
-        end
         if (reg_write_ex2mem_ff && (rd_ex2mem_ff != 'd0) && (rd_ex2mem_ff == rs1_id2ex_ff)) begin
             fwd_a = 2'b10;
         end
+        else if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs1_id2ex_ff)) begin
+            fwd_a = 2'b01;
+        end
+        else begin
+            fwd_a = 2'b00;
+        end
     end
 
-    // blocked assignment
-    // the second if-statement will cover the first one, if condition is true
     always_comb begin
-        fwd_b = 2'b00;
-        if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs2_id2ex_ff)) begin
-            fwd_b = 2'b01;
-        end
         if (reg_write_ex2mem_ff && (rd_ex2mem_ff != 'd0) && (rd_ex2mem_ff == rs2_id2ex_ff)) begin
             fwd_b = 2'b10;
+        end
+        else if (reg_write_mem2wb_ff && (rd_mem2wb_ff != 'd0) && (rd_mem2wb_ff == rs2_id2ex_ff)) begin
+            fwd_b = 2'b01;
+        end
+        else begin
+            fwd_b = 2'b00;
         end
     end
 
